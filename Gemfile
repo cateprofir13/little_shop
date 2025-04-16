18367aed92d427ec820c915c5b05385a5303f03a
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-16 16:06:41 -0700

    fixed skeleton

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem "simplecov"
   gem "pry"
   gem "pgreset"
+  gem 'jsonapi-serializer'
   gem 'shoulda-matchers'
 end
 
---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class ItemsController < BaseController
+      def index
+        
+      end
+      
+      def show
+        
+      end
+      
+      def create
+        
+      end
+      
+      def update
+        
+      end
+      
+      def destroy
+        
+      end
+
+      private
+      
+      def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      end
+    end
+  end
+end
