7279c147ac78476face77227d08ef8254088d6e9
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-04-16 16:00:26 -0500

    Created the relationships

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Merchant, type: :model do
+  describe "relationships" do
+    it { should have_many(:items) }
+    it { should have_many(:invoices) }
+  end
+end
