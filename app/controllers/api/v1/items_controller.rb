31c6d37ce24937ec53fb2e409db6176499fbbd8e
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-16 16:11:54 -0700

    updated skeleton (final)

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class ItemsController < BaseController
+    class ItemsController < ApplicationController
       def index
         
       end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class MerchantsController < BaseController
+    class MerchantsController < ApplicationController
       
     end
   end
