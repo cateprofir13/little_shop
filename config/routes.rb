706d9ae8b7f5a86804cf1a514a9b319be66b31a0
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-16 15:38:29 -0700

    endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,33 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
+  # Merchant endpoints
+  get "/api/v1/merchants", to: "api/v1/merchants#index"
+  get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
+  post "/api/v1/merchants", to: "api/v1/merchants#create"
+  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
+  delete "/api/v1/merchants/:id", to: "api/v1/merchants#destroy"
+  
+  # Merchant nested endpoints
+  get "/api/v1/merchants/:merchant_id/items", to: "api/v1/merchant_items#index"
+  get "/api/v1/merchants/:merchant_id/customers", to: "api/v1/merchant_customers#index"
+  get "/api/v1/merchants/:merchant_id/invoices", to: "api/v1/merchant_invoices#index"
+  
+  # Item endpoints
+  get "/api/v1/items", to: "api/v1/items#index"
+  get "/api/v1/items/:id", to: "api/v1/items#show"
+  post "/api/v1/items", to: "api/v1/items#create"
+  patch "/api/v1/items/:id", to: "api/v1/items#update"
+  delete "/api/v1/items/:id", to: "api/v1/items#destroy"
+  
+  # Item nested endpoints
+  get "/api/v1/items/:item_id/merchant", to: "api/v1/item_merchants#show"
+  
+  # Search endpoints
+  get "/api/v1/merchants/find", to: "api/v1/merchants/search#show"
+  get "/api/v1/items/find_all", to: "api/v1/items/search#index"
+  
   # Defines the root path route ("/")
   # root "posts#index"
 end
