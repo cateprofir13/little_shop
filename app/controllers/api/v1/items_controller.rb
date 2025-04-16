8e1b03c93b473d9578eb211a821022fc93be80ff
Brady Blair <bblair321@yahoo.comm>
2025-04-16 16:32:03 -0700

    completed items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,23 +2,25 @@ module Api
   module V1
     class ItemsController < ApplicationController
       def index
-        
+        render json: ItemSerializer.new(Item.all)
       end
       
       def show
-        
+        render json: ItemSerializer.new(Item.find(params[:id]))
       end
       
       def create
-        
+        render json: ItemSerializer.new(Item.create!(item_params[:item])), status: :created
       end
       
       def update
-        
+        render json: ItemSerializer.new(Item.update!(item_params[:item])), status: :ok
       end
       
       def destroy
-        
+        item = Item.find(params[:id])
+        item.destroy
+        head :no_content
       end
 
       private
