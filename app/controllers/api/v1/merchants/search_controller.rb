813e69389ba4de232804d3faa0a9facc6cfd073e
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-17 15:25:06 -0700

    find all merchants

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -19,6 +19,20 @@ module Api
             render json: MerchantSerializer.new(merchant)
           end
         end
+
+        def index
+          if params[:name].blank?
+            render json: { 
+              message: "your query could not be completed", 
+              errors: ["No valid search parameter"] 
+            }, status: :bad_request
+            return
+          end
+          
+          merchants = find_all_merchants
+          
+          render json: MerchantSerializer.new(merchants)
+        end
         
         private
         
@@ -29,6 +43,14 @@ module Api
         def find_merchant
           if params[:name].present?
             Merchant.where('lower(name) ILIKE ?', "%#{params[:name].downcase}%").first
+          end
+        end
+        
+        def find_all_merchants
+          if params[:name].present?
+            Merchant.where('lower(name) ILIKE ?', "%#{params[:name].downcase}%")
+          else
+            []
           end
         end
       end
