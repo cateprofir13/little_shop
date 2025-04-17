310f20d0c6b2f5f546ef04c5b5bcfa0e1c7f4c40
Calvin Suiter <cdsuiter@yahoo.com>
2025-04-17 19:27:08 -0400

    Added functionality for merchant invoices controller

---
+++ app/controllers/api/v1/merchant_invoices_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+      class MerchantInvoicesController < ApplicationController
+				def index
+					merchant = Merchant.find(params[:merchant_id])
+                    invoices = merchant.invoices
+					render json: InvoiceSerializer.new(invoices).serializable_hash
+				end
+	   	end
+		end
+	end
