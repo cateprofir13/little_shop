5c1e0ba0bde4a36e65ca6f2f0ca06d65b3ad6de7
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-04-18 08:35:48 -0500

    added rescue from functionaity for the merchant customer  controler

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -1,12 +1,19 @@
 module Api
 	module V1
 		class MerchantCustomersController < ApplicationController
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 			def index
 						merchant = Merchant.find(params[:merchant_id])
 						customer_ids = merchant.invoices.pluck(:customer_id).uniq
 						customers = Customer.where(id: customer_ids)
 						render json: CustomerSerializer.new(customers).serializable_hash
 			end
+      
+      private
+
+      def record_not_found(error)
+        render json: { errors: [error.message] }, status: :not_found
+      end
 		end
 	end
 end
--- app/controllers/api/v1/merchant_invoices_controller.rb
+++ app/controllers/api/v1/merchant_invoices_controller.rb
@@ -1,11 +1,18 @@
 module Api
 	module V1
 		class MerchantInvoicesController < ApplicationController
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 			def index
 					merchant = Merchant.find(params[:merchant_id])
 					invoices = merchant.invoices
 					render json: InvoiceSerializer.new(invoices).serializable_hash
 			end
+
+      private
+
+      def record_not_found(error)
+        render json: { errors: [error.message] }, status: :not_found
+      end
 		end
 	end
 end
