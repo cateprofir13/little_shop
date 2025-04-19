5928062564882ee5fd499802b047df0118f467a2
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-04-19 13:24:16 -0500

    Refactor

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,11 @@ has_many :invoices
   def self.sorted_by_created_at(order = "desc")
     %w[asc desc].include?(order.downcase) ? order(created_at: order.downcase) : all
   end
+
+
+  def self.with_returned_items
+    joins(items: { invoice_items: :invoice })
+      .where(invoices: { status: 'returned' })
+      .distinct
+  end
 end
