988d453bcb81d217ca9f49c85f7bd86b3d5d11dd
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-04-20 19:55:33 -0500

    updated the serializer to only render item count on request

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -3,7 +3,9 @@ class MerchantSerializer
   
   attributes :name
 
-  attribute :item_count do |merchant|
-   merchant.item_count
-  end
+  attribute :item_count, if: proc { |_record, params|
+  params && params[:include_item_count]
+} do |merchant|
+  merchant.item_count
+end
 end
