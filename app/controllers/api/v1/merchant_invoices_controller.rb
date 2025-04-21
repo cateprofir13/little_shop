200348fa36477a5f0f90996cfae2fa434b97633a
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-21 11:32:22 -0700

    refactoring merges #2

--- app/controllers/api/v1/merchant_invoices_controller.rb
+++ app/controllers/api/v1/merchant_invoices_controller.rb
@@ -1,18 +1,11 @@
 module Api
-	module V1
-		class MerchantInvoicesController < ApplicationController
-      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-			def index
-					merchant = Merchant.find(params[:merchant_id])
-					invoices = merchant.invoices
-					render json: InvoiceSerializer.new(invoices).serializable_hash
-			end
-
-      private
-
-      def record_not_found(error)
-        render json: { errors: [error.message] }, status: :not_found
+  module V1
+    class MerchantInvoicesController < BaseController
+      def index
+        merchant = Merchant.find(params[:merchant_id])
+        invoices = merchant.invoices
+        render json: InvoiceSerializer.new(invoices).serializable_hash
       end
-		end
-	end
+    end
+  end
 end
